d777ea9bdd13bfe8551816c7d6ee2a2e4c2cb30f
sallyNader <s.ahmad@vardot.com>
2017-12-10 10:27:37 +0200

    create header , footer and home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {RouterModule,Routes}  from '@angular/router';
 import { AppComponent } from './app.component';
-
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+const appRoutes:Routes =[
+  {path :"home" , component: HomeComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
